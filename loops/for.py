3da26b65527a43d1b8d72cec0ae4eee6c2ac7363
Aamir Majeed <aamirmaj@gmail.com>
2023-08-29 18:35:28 -0400

    Add prints to for.py

--- loops/for.py
+++ loops/for.py
@@ -4,6 +4,9 @@ numbers = [1,2,3,4,5]
 for i in numbers:
   print(i)
 
+print(list(range(10)))
+print(list(range(5,10)))
+
 # we can also use "range" to get a list of numbers
 numbers = range(1,6) # equals 1 through 5
 for i in numbers:
