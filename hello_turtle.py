f054e0cffd2b67e52f9ceaaa310a640faae10475
Aamir Majeed <aamirmaj@gmail.com>
2023-08-29 19:32:21 -0400

    Update hello_turtle.py

--- hello_turtle.py
+++ hello_turtle.py
@@ -2,13 +2,17 @@ from turtle import *
 
 my_turtle = Turtle()
 
-# my_turtle.speed(1)
-# my_turtle.right(90)
-# my_turtle.forward(100)
-
 # 1. Make the turtle move in a circle/square
+# my_turtle.circle(50)
 
 # 2. Make the turtle loop 5 times
+
+my_turtle.speed(1)
+for i in range(1,6):
+    my_turtle.forward(50)
+    for j in range(2,10):
+        my_turtle.right(30)
+        my_turtle.forward(10)
 
 # 3. Make the turtle loop forever
 
